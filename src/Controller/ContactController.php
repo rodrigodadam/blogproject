a147d006ce7efe13e24255dd00269effb8f27158
Rodrigo Dadam <rodrigodadam@gmail.com>
2019-11-23 21:11:35 +0000

    ContactController created

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact()
+    {
+        return $this->render('contact/contact.html.twig', [
+            'controller_name' => 'ContactController',
+        ]);
+    }
+}
