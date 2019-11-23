62d89cb73e3a87b6beae7b37c348491714ab432a
Rodrigo Dadam <rodrigodadam@gmail.com>
2019-11-23 20:49:49 +0000

    first commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+class HomeController extends AbstractController
+{
+    public function index()
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}
