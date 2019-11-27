6badcef7fb21b9bbfd3ed0ad1dee754722b70c9f
Rodrigo Dadam <rodrigodadam@gmail.com>
2019-11-27 15:55:20 +0000

    admin htmls

--- src/Controller/admin/AuthorController.php
+++ src/Controller/admin/AuthorController.php
@@ -6,24 +6,74 @@ namespace App\Controller\admin;
 
 use App\Entity\Author;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuthorController extends AbstractController
 {
 
-    public function newAuthor()
+    //TODO: Change after
+
+    public function index()
     {
-        $entityManager = $this->getDoctrine()->getManager();
+        $authors = $this->getDoctrine()->getRepository(
+            Author::class)->findAll();
 
-        $author_mock = new Author();
-        $author_mock->setName('Rodrigo');
-        $author_mock->setEmail('rodrigo@gmail.com');
-        $author_mock->setAbout('About the author 01');
-
-        $entityManager->persist($author_mock);
-        $entityManager->flush();
-
-        return new Response('Saves a new author in database');
+        return $this->render('admin/index.html.twig', array(
+            'authors' => $authors
+        ));
     }
+
+    public function newAuthor(Request $request)
+    {
+        $author = new Author();
+
+        $form = $this->createFormBuilder($author)
+            ->add('name', TextType::class, array(
+                'required' => false,
+                'attr' => array('class' => 'form-control')))
+            ->add('email', EmailType::class, array(
+                'required' => false,
+                'attr' => array('class' => 'form-control')))
+            ->add('about', TextareaType::class, array(
+                'required' => false,
+                'attr' => array('class' => 'form-control')
+            ))
+            ->add('save', SubmitType::class, array(
+                'label' => 'Create',
+                'attr' => array('class' => 'btn btn-primary mt-3')
+            ))
+            ->getForm();
+
+        return $this->render('admin/new-author.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+    public function authorInfo($id)
+    {
+        $author = $this->getDoctrine()->getRepository(Author::class)->find($id);
+
+        return $this->render('admin/info.html.twig', array('author' => $author));
+    }
+
+//    public function newAuthor()
+//    {
+//        $entityManager = $this->getDoctrine()->getManager();
+//
+//        $author_mock = new Author();
+//        $author_mock->setName('Leticia');
+//        $author_mock->setEmail('leticia@gmail.com');
+//        $author_mock->setAbout('About the author 02');
+//
+//        $entityManager->persist($author_mock);
+//        $entityManager->flush();
+//
+//        return new Response('Saves a new author in database');
+//    }
 
 }
