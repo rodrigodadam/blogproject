1ba5741ed9cfe69e0a77a22f53025f3a6bc8aa68
Rodrigo Dadam <rodrigodadam@gmail.com>
2019-11-27 01:11:32 +0000

    ContactAction implementation

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,18 +2,51 @@
 
 namespace App\Controller;
 
+use App\Entity\ContactUs;
+use Doctrine\DBAL\Connection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+
+
 
 class ContactController extends AbstractController
 {
-    /**
-     * @Route("/contact", name="contact")
-     */
     public function contact()
     {
         return $this->render('contact/contact.html.twig', [
             'controller_name' => 'ContactController',
         ]);
     }
+
+    public function contactAction(Request $request)
+    {
+        $form_data = $request->request->all();
+//        $form_data_mock = [
+//            'name' => 'title',
+//            'email' => '/src/images'
+//            'subject' => '/src/images'
+//            'message' => '/src/images'
+//        ];
+
+        //TODO: validate fields
+
+        $contactus = new ContactUs();
+        $contactus->setName($form_data['name']);
+        $contactus->setEmail($form_data['email']);
+        $contactus->setSubject($form_data['subject']);
+        $contactus->setMessage($form_data['message']);
+        $contactus->setCreatedAt(new \DateTime('NOW'));
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($contactus);
+        $entityManager->flush();
+
+        //TODO: Create js banner with confirmation
+
+        return $this->render('contact/contact.html.twig', [
+            'controller_name' => 'ContactController',
+        ]);
+    }
+
 }
---
+++ src/Entity/ContactUs.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ContactUsRepository")
+ */
+class ContactUs
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=180)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $subject;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $message;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+    /**
+     * @param mixed $subject
+     */
+    public function setSubject($subject): void
+    {
+        $this->subject = $subject;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * @param mixed $message
+     */
+    public function setMessage($message): void
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param mixed $createdAt
+     */
+    public function setCreatedAt($createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
