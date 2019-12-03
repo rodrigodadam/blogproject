f8d7899938414bdce14e7767f781932cc5a71fe8
Rodrigo Dadam <rodrigodadam@gmail.com>
2019-12-03 19:25:50 +0000

    entities reformulation

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -94,7 +94,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('register'));
+        return new RedirectResponse($this->urlGenerator->generate('/admin'));
 
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
